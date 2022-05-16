bc7516f8c861948772a9f4c2e6659b69a2c24857
Kareem Morsy <kreem.morsy@hotmail.com>
2022-05-17 01:18:00 +0200

    Problem 2 trial
    Co-authored-by: Mohamed Ashraf <ashraf.dawood2295@gmail.com>
    Co-authored-by: dovaaa <dovaaa@users.noreply.github.com>

--- manga_solutions.pl
+++ manga_solutions.pl
@@ -1,7 +1,7 @@
 move([A, B, C | T], NewState):-
     availableMoves([A, B, C | T], Moves),
     member(N, Moves),
-    removeElement([A, B, C | T], N, NewState).
+    removeElementByIndex([A, B, C | T], N, NewState).
 
 availableMoves(List, Output):-
     availableMoves(List, 0, Output).
@@ -10,11 +10,11 @@ availableMoves([_|T], N, [N | Output]):-
     Q is N+1,
     availableMoves(T, Q, Output).
 
-removeElement([_|T], 0, T).
+removeElementByIndex([_|T], 0, T).
 
-removeElement([H|T], N, [H | NewState]):-
+removeElementByIndex([H|T], N, [H | NewState]):-
     Q is N - 1,
-    removeElement(T, Q, NewState).
+    removeElementByIndex(T, Q, NewState).
 
 
 :-use_module(library(lists)).
@@ -55,3 +55,100 @@ moves( State, Open, Closed,[Next,State]):-
 % Problem 1 Informed
 
 % Problem 2 Informed
+
+
+%Remove first occurrence
+removeElementByValue([], _, []).
+removeElementByValue([Elem|T], Elem, T):-!.
+removeElementByValue([H|T], Elem, [H|Next]):-
+    removeElementByValue(T, Elem, Next).
+
+moveDeletiveEditing(State, NewState):-
+    member(N, State),
+    removeElementByValue(State, N, NewState).
+
+%get difference in place
+
+
+getHeuristic([H|T1],N,Goal):-
+    not(member(H, Goal)),
+    getHeuristic(T1,V, Goal),
+    N is V + 1.
+
+getHeuristic([H|T1],N,Goal):-
+    member(H, Goal),
+    getHeuristic(T1,N, Goal). 
+    
+getHeuristic([], 0, _). 
+
+%Get Best Child
+%State Representation --> [State, Parent, H]
+
+getBest([State, Parent, H], [_, _, H1], [State, Parent, H]):-
+	H < H1, !.
+getBest([_, _, _], [State1, Parent1, H1], [State1, Parent1, H1]).
+
+getBestChild1([State], State):-!.
+getBestChild1([State | Rest], Best):-
+    getBestChild1(Rest, Temp),
+    getBest(State, Temp, Best).
+
+
+getBestChild([Child], Child, []).
+getBestChild(Open, Best, RestOpen):-
+    getBestChild1(Open, Best),
+    removeElementByValue(Best, Open, RestOpen).
+
+
+
+%%%%%%DOVA
+    %%case child not opened or closed
+
+    % getBestChilds(Open,[State,P])
+    %case child already open 
+    %case child already closed 
+
+
+%%%%%%%
+
+
+
+getChildrenDeletive(State, Open ,Closed, Children, H, Goal):-
+    bagof(X, movesDeletive( State, Open, Closed, X, H, Goal), Children).
+getChildrenDeletive(_,_,_, [],_,_).
+
+movesDeletive(State, Open, Closed, [Next,State,H], H, Goal):-
+    moveDeletiveEditing(State, Next),
+    \+ member(State, Open),
+    \+ member(State, Closed),
+    getHeuristic(Next, H, Goal).
+
+
+% Get Path
+
+pathDeletiveEditing(Open, Closed, Goal):-
+	getBestChild(Open, [Goal, Parent, H], RestOfOpen),
+    printsolution([Goal,Parent, H], Closed),!.
+    
+pathDeletiveEditing(Open, Closed, Goal):-
+    getBestChild(Open, [State,Parent , H], RestOpen),
+    getChildrenDeletive(State, Open, Closed, Children, H, Goal),
+    addListToOpenBFS(Children , RestOpen, NewOpen),
+	pathDeletiveEditing(NewOpen, [[State,Parent , H] | Closed], Goal).
+   
+deletiveEditing(Start, Goal):-
+    getHeuristic(Start, H, Goal),
+    pathDeletiveEditing([[Start,null, H]],[], Goal).
+
+addListToOpenBFS(Children, [], Children).
+addListToOpenBFS(Children, [H|Open], [H|NewOpen]):-
+		addListToOpenBFS(Children, Open, NewOpen).
+
+printsolution([State, null, H],_):-!,
+		write(State), write(' H:'), write(H), nl.
+printsolution([State, Parent, H], Closed):-
+		member([Parent, GrandParent, PC1, H1, TC1], Closed),
+		printsolution([Parent, GrandParent, PC1, H1, TC1], Closed),
+		write(State), write(' H:'), write(H),  nl.
+
+%%problems might be moving or getting the best child or assigning heuristics
